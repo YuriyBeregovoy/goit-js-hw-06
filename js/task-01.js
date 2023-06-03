50dfa9bab222778bdfd28c01f2e3060ff5caa772
YuriyBeregovoy <rik0681@gmail.com>
2023-06-03 22:27:08 +0300

    added task code number 1

---
+++ js/task-01.js
@@ -0,0 +1,20 @@
+const categoriesEl = document.querySelector("#categories");
+
+const numberOfCategoriesEl = categoriesEl.children;
+
+console.log(`Number of categories: ${numberOfCategoriesEl.length}`);
+console.log('\n');
+
+const numberOfCategoriesArray = [...numberOfCategoriesEl];
+
+numberOfCategoriesArray.forEach(function ({ firstElementChild, lastElementChild }) {
+  
+  console.log(`Category: ${firstElementChild.textContent}`);
+
+  const listCategoryItems = lastElementChild.children;
+  const listCategoryArrey = [...listCategoryItems];
+
+  console.log(`Elements: ${listCategoryArrey.length}`)
+  console.log('\n');
+
+});
