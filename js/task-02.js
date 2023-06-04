e06bb653b10164462358a9ab58e775fdbfd162c4
YuriyBeregovoy <rik0681@gmail.com>
2023-06-04 11:13:43 +0300

    added task code number 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listItemElements = ingredients.map(ingredient => {
+  const listItemEl = document.createElement("li");
+  
+  listItemEl.classList.add("item")
+  listItemEl.textContent = ingredient;
+  
+    return listItemEl;
+})
+
+const listEl = document.querySelector("#ingredients");
+  listEl.append(...listItemElements);
