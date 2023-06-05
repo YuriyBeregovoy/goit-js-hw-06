02500e7127c8e59e0ef579fe4222e29254d4d12c
YuriyBeregovoy <rik0681@gmail.com>
2023-06-05 17:44:50 +0300

    task 9 is completed

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  
 }
+
+const bodyBckgColorEl = document.querySelector("body");
+const buttonChangeColorEl = document.querySelector(".change-color");
+const nameBckgColorEl = document.querySelector(".color");
+
+buttonChangeColorEl.addEventListener('click', function () {
+  nameBckgColorEl.textContent = getRandomHexColor();
+  bodyBckgColorEl.style.backgroundColor = getRandomHexColor();
+});
+
