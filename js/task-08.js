ae38f8dbf2c65ea34dcc5db46f998bf23c22214b
YuriyBeregovoy <rik0681@gmail.com>
2023-06-05 17:12:44 +0300

    task 8 is completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginFormEl = document.querySelector(".login-form");
 
+loginFormEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+   if (email === "" || password === "") {
+    return alert("Please fill in all the fields!");
+   }
+  
+    const formData = {
+    email,
+    password,
+  }
+  console.log(formData)
+ event.currentTarget.reset();
+
+ }
