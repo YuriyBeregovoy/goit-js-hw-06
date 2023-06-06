5ab92185c5441e9ee6bba0c2d8fc109fb9bf16ac
YuriyBeregovoy <rik0681@gmail.com>
2023-06-06 12:07:32 +0300

    the fixed bugs of the task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,12 @@
 const formEl = document.querySelector("#name-input");
+
 const nameEl = document.querySelector("#name-output");
-formEl.addEventListener('input', (event) => {nameEl.textContent = event.currentTarget.value;});
+
+formEl.addEventListener('input', (event) => {
+  if (event.currentTarget.value === '') {
+    nameEl.textContent = 'Anonymous';
+  } else {
+    nameEl.textContent = event.currentTarget.value;
+  }
+});
 
