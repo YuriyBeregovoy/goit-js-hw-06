3462a229b1f7112e3e7b828a0b42fb0bed81854a
YuriyBeregovoy <rik0681@gmail.com>
2023-06-06 12:54:42 +0300

    the fixed bugs of the task 1

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const categoriesEl = document.querySelector("#categories");
 
 const numberOfCategoriesEl = categoriesEl.children;
 
-console.log(`Number of categories: ${numberOfCategoriesEl.length}`);
+console.log("Number of categories:", numberOfCategoriesEl.length);
 console.log('\n');
 
 const numberOfCategoriesArray = [...numberOfCategoriesEl];
@@ -14,7 +14,7 @@ numberOfCategoriesArray.forEach(function ({ firstElementChild, lastElementChild 
   const listCategoryItems = lastElementChild.children;
   const listCategoryArrey = [...listCategoryItems];
 
-  console.log(`Elements: ${listCategoryArrey.length}`)
+  console.log("Elements:", listCategoryArrey.length)
   console.log('\n');
 
 });
