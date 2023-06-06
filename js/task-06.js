2ad3658c2773d55417061d69fe1d8c51519fa7e7
YuriyBeregovoy <rik0681@gmail.com>
2023-06-06 11:51:20 +0300

    the fixed bugs of the task 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,6 +2,7 @@ const textInputEl = document.querySelector("#validation-input");
 const dataLength = Number(textInputEl.dataset.length);
 
 textInputEl.addEventListener("blur", (event) => {
- const inputValueLength = event.currentTarget.value.length;
- inputValueLength === dataLength ? textInputEl.classList.add("valid") : textInputEl.classList.add("invalid"); 
+  const inputValueLength = event.currentTarget.value.length;
+  textInputEl.classList.toggle("valid", inputValueLength === dataLength);
+  textInputEl.classList.toggle("invalid", inputValueLength !== dataLength);
 });
